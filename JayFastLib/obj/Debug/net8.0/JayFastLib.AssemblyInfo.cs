58c5473bfcc6a47332210aba0bea1ccf4edf577a
jaydenhuang555 <jaydenkanahuang@outlook.com>
2025-05-18 23:32:05 -0700

    updated

--- JayFastLib/obj/Debug/net8.0/JayFastLib.AssemblyInfo.cs
+++ JayFastLib/obj/Debug/net8.0/JayFastLib.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JayFastLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+300e583f85d20bd47aad9103fb1f081ad917e50a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fdf62c3c1a418f050c3aafa72b95b962d3fb8a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("JayFastLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JayFastLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
